86a25ef5082de8271db9260463edcfefe7e2a374
kalyan <kalyan.kbrs@gmail.com>
2019-06-22 22:17:20 +0530

    First Commit

---
+++ precache-manifest.f1164123e6e2b4723fb7f64814f178aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "946b60c610547fd5da4eea184ad5eb3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "662ee34d919c4b17c50a",
+    "url": "/static/css/2.fa4a79a6.chunk.css"
+  },
+  {
+    "revision": "baf58391d413f62f9712",
+    "url": "/static/css/main.e3dffd5e.chunk.css"
+  },
+  {
+    "revision": "662ee34d919c4b17c50a",
+    "url": "/static/js/2.de736759.chunk.js"
+  },
+  {
+    "revision": "baf58391d413f62f9712",
+    "url": "/static/js/main.361cd6b1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
